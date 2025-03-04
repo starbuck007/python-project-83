c667b26910767570a6f083cb037b3fd314e312fc
Regina Khairutdinova <regina.khajrutdinova@gmail.com>
2025-03-04 23:17:20 +0400

    add index.html and logic for page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/starbuck007/python-project-83/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/starbuck007/python-project-83/actions)
+
+Приложение доступно по ссылке: [Page Analyzer](https://python-project-83-i00p.onrender.com/)
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return 'Hello, Hexlet!'
+    if request.method == "POST":
+        url = request.form["url"]
+        return f"Проверяем {url}..."
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
