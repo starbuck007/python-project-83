781e3a509cc87fbe0c0bb21424239f619c7d7a82
Regina Khairutdinova <regina.khajrutdinova@gmail.com>
2025-03-26 20:52:23 +0400

    fix build

--- build.sh
+++ build.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
-set -e
 
-if [ -f .env ]; then
-    while IFS= read -r line || [ -n "$line" ]; do
-        if [[ ! -z "$line" && ! "$line" =~ ^# ]]; then
-            key=$(echo "$line" | cut -d= -f1)
-            value=$(echo "$line" | cut -d= -f2-)
-            export "$key=$value"
-        fi
-    done < .env
-fi
-
-echo ">>> Устанавливаем uv"
-curl -LsSf https://astral.sh/uv/install.sh | sh || echo "Ошибка при установке uv"
-
-echo ">>> Загружаем окружение"
-export PATH="$HOME/.local/bin:$PATH"
-
-echo ">>> Устанавливаем зависимости"
-uv sync || echo "Ошибка при установке зависимостей"
-
-echo ">>> Создаем таблицы в базе данных"
-psql "$DATABASE_URL" -f database.sql || echo "Ошибка при создании таблиц"
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source ${PWD}/.env
+make install && psql -a -d $DATABASE_URL -f database.sql
